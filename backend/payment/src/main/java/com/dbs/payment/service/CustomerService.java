f8dbd200c63f9212d49441d051fb858ccf40aa66
BheemReddySoumyaReddy25 <bheemreddysoumyareddy@gmail.com>
2022-07-28 06:38:20 +0000

    Customer Service

--- backend/payment/src/main/java/com/dbs/payment/service/CustomerService.java
+++ backend/payment/src/main/java/com/dbs/payment/service/CustomerService.java
@@ -2,8 +2,12 @@ package com.dbs.payment.service;
 
 import java.util.List;
 
+import com.dbs.payment.exception.CustomerNotFoundException;
 import com.dbs.payment.model.Customer;
 
 public interface CustomerService {
 	public List<Customer> getCustomers();
+	public Customer getCustomerById(int id) throws CustomerNotFoundException;
+	public String saveCustomer(Customer customer);
+	public String deleteCustomerById(int id) throws CustomerNotFoundException;
 }
--- backend/payment/src/main/java/com/dbs/payment/service/CustomerServiceImplementation.java
+++ backend/payment/src/main/java/com/dbs/payment/service/CustomerServiceImplementation.java
@@ -1,11 +1,13 @@
 package com.dbs.payment.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dbs.payment.dao.CustomerRepository;
+import com.dbs.payment.exception.CustomerNotFoundException;
 import com.dbs.payment.model.Customer;
 
 @Service
@@ -14,8 +16,25 @@ public class CustomerServiceImplementation implements CustomerService{
 	private CustomerRepository customerRepository;
 	@Override
 	public List<Customer> getCustomers() {
-		// TODO Auto-generated method stub
 		return customerRepository.findAll();
+	}
+	@Override
+	public Customer getCustomerById(int id) throws CustomerNotFoundException {
+		Optional<Customer> res = customerRepository.findById(id);
+		if(res.isEmpty())
+			throw new CustomerNotFoundException("Invalid customer id - "+id);
+		return res.get();
+	}
+	@Override
+	public String saveCustomer(Customer customer) {
+		customerRepository.save(customer);
+		return "sucessfully saved customer details";
+	}
+	@Override
+	public String deleteCustomerById(int id) throws CustomerNotFoundException {
+		Customer customer = getCustomerById(id);
+		customerRepository.delete(customer);
+		return "sucessfully deleted customer with id - "+id;
 	}
 
 }
