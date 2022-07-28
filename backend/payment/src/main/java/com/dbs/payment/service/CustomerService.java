1378f9edef0bd9d7d5227035d999b5d2aa33081e
BheemReddySoumyaReddy25 <bheemreddysoumyareddy@gmail.com>
2022-07-28 09:25:36 +0000

    added model mapper in service layer

--- backend/payment/src/main/java/com/dbs/payment/service/CustomerService.java
+++ backend/payment/src/main/java/com/dbs/payment/service/CustomerService.java
@@ -2,12 +2,12 @@ package com.dbs.payment.service;
 
 import java.util.List;
 
+import com.dbs.payment.dto.CustomerDTO;
 import com.dbs.payment.exception.CustomerNotFoundException;
-import com.dbs.payment.model.Customer;
 
 public interface CustomerService {
-	public List<Customer> getCustomers();
-	public Customer getCustomerById(int id) throws CustomerNotFoundException;
-	public String saveCustomer(Customer customer);
+	public List<CustomerDTO> getCustomers();
+	public CustomerDTO getCustomerById(int id) throws CustomerNotFoundException;
+	public String saveCustomer(CustomerDTO customer);
 	public String deleteCustomerById(int id) throws CustomerNotFoundException;
 }
