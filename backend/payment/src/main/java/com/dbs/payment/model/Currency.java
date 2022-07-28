8c91511fca508f49d611825ffea1f679ff705223
BheemReddySoumyaReddy25 <bheemreddysoumyareddy@gmail.com>
2022-07-28 09:24:59 +0000

    removed unused import tags

--- backend/payment/src/main/java/com/dbs/payment/model/Currency.java
+++ backend/payment/src/main/java/com/dbs/payment/model/Currency.java
@@ -6,10 +6,7 @@ import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data @NoArgsConstructor @AllArgsConstructor
 @Entity
