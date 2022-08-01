542095638e2a7337cf554887d661d67d269ce4d0
BheemReddySoumyaReddy25 <bheemreddysoumyareddy@gmail.com>
2022-08-01 11:23:16 +0000

    update employee id type to big int

--- backend/payment/src/main/java/com/dbs/payment/model/Transaction.java
+++ backend/payment/src/main/java/com/dbs/payment/model/Transaction.java
@@ -30,30 +30,30 @@ public class Transaction {
 	@Column(length = 11)
 	private int transactionId;
 	@ManyToOne(fetch = FetchType.LAZY,
-			cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+			cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
 	@JoinColumn(name="customer_id")
 	private Customer customer;
 	@ManyToOne(fetch = FetchType.LAZY,
-			cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+			cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
 	@JoinColumn(name="currency_code")
 	private Currency currency;
 	@ManyToOne(fetch = FetchType.LAZY,
-			cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+			cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
 	@JoinColumn(name="sender_bic")
 	private Bank senderBIC;
 	@ManyToOne(fetch = FetchType.LAZY,
-			cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+			cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
 	@JoinColumn(name="receiver_bic")
 	private Bank receiverBIC;
 	@Column(length = 11)
 	private String receiverAccountHolderNumber;
 	private String receiverAccountHolderName;
 	@ManyToOne(fetch = FetchType.LAZY,
-			cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+			cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
 	@JoinColumn(name="transfer_type_code")
 	private TransferType transferType;
 	@ManyToOne(fetch = FetchType.LAZY,
-			cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+			cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
 	@JoinColumn(name="message_code")
 	private Message message;
 	private double currencyAmount;
