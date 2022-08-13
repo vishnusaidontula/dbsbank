e976db65fbe28a40ff3c19aeab45f0ae56542f37
BheemReddySoumyaReddy25 <bheemreddysoumyareddy@gmail.com>
2022-08-13 05:02:06 +0000

    organisms dropdown

---
+++ frontend/payment/src/components/organisms/DropDown/DropDown.js
@@ -0,0 +1,41 @@
+import React,{useState,useEffect} from 'react'
+import Select from 'react-select'
+import getList from '../../../services/getList';
+const DropDown = (props) => {
+    const [value,setValue] = useState("");
+    const [filterValues,setFilterValues] = useState([]);
+    useEffect(() => {
+         async function getData(){
+            await getList(props.url).then(
+                (res)=>{
+                    console.log(`inside get list api of value ${props.url}`+res);
+                    setFilterValues(res);
+                }
+            )
+            .catch(
+                (err)=>{console.log(err)}
+            )
+        }
+        getData()
+    },[props.url])
+    const handleInputChange = (event)=>{
+        setValue(event.target.value);
+        props.getValue(value);
+    }
+    const getOptions = ()=>{
+        const options = [];
+        filterValues.forEach(
+            data =>{
+                options.push({value:data,label:data[props.getById]})
+            } 
+        )
+        return options;
+    }
+    return (
+        <div>
+            <Select options={getOptions()} onChange={handleInputChange} />
+        </div>
+    )
+}
+
+export default DropDown
