df001bd49cac748626bf2584f6b4b42a2a9bc4fc
BheemReddySoumyaReddy25 <bheemreddysoumyareddy@gmail.com>
2022-08-14 07:17:56 +0000

    view transaction

--- frontend/payment/src/App.js
+++ frontend/payment/src/App.js
@@ -1,9 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ViewTransaction from "./components/organisms/view transaction/ViewTransaction.js";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Routes>
+          <Route path="/transaction/:id" element={<ViewTransaction />}></Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
